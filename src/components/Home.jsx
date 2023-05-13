aeb0729e5fe1320ae7990f3dadd8587a5108f5e2
rohit <rhtry.tech@gmail.com>
2023-05-14 00:47:10 +0530

    add : files

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/home.css";
+
+const Home = () => {
+  return (
+    <div className="main_container">
+      <p className="p_mainText">Rohit Roy | Home</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Meet.jsx
@@ -0,0 +1,11 @@
+import {useEffect} from "react";
+
+const Meet = () => {
+  useEffect(() => {
+    window.location.href = "https://meet.google.com/jnh-xcvi-idr";
+  }, []);
+
+  return null;
+};
+
+export default Meet;
